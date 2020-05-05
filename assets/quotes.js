ae38d06e108e2ea1598ef4708940b69b6e3f2c94
burhan1204 <58620329+burhan1204@users.noreply.github.com>
2020-05-05 16:20:26 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "i see you for the first time" },
-    { author: "standinshd", quotes: "Hey! I miss you! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you salsa." },
+    { author: "29<3", quotes: "Whatever happens, keep breathing" },
+    { author: "29<3", quotes: "i see you for the first time" },
+    { author: "29<3", quotes: "Hey! I miss you! Salsa" },
+    { author: "29<3", quotes: "I love you." },
+    { author: "29<3", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "29<3", quotes: "I love you." },
+    { author: "29<3", quotes: "I'm sorry for loving you salsa." },
 ];
